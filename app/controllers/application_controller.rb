1275a16922ace8352c3aea8965b0e0b564e4de5a
CBrashear <chrisbrashear90@yahoo.com>
2022-06-30 14:05:25 -0500

    Add start/stop logging to each request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  before_action :print_request
+
+  after_action :print_response
+
+  private
+  
+    def print_request
+      logger.debug "Starting request: " + request.original_url
+
+      request.headers.each do |k,v|
+        logger.debug "HTTP Headers: [" + k.to_s + ", " + v.to_s + "]" if k.starts_with? 'HTTP'
+      end
+    end
+
+    def print_response
+      logger.debug "Request complete, sent body: " + response.body
+    end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,6 @@
 class Team < ApplicationRecord
-  has_many :players, :coaches
+  has_many :players
+  has_many :coaches
   
   validates :name, presence: true
 end
